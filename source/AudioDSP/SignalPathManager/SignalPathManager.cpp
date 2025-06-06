0eb5e33af38e9cab5ba425512dc58ef63fa03fc6
smoke <gboling829@gmail.com>
2025-06-06 13:34:20 -0400

    crystallizer-24 Create a signal path manager class to organize ProcessorChains and handle paralle...
    began work on implementing a helper class to manage the signal path

---
+++ source/AudioDSP/SignalPathManager/SignalPathManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by smoke on 6/6/2025.
+//
+
+#include "SignalPathManager.h"
---
+++ source/AudioDSP/SignalPathManager/SignalPathManager.h
@@ -0,0 +1,37 @@
+//
+// Created by smoke on 6/6/2025.
+//
+
+#pragma once
+
+#ifndef SIGNALPATHMANAGER_H
+#define SIGNALPATHMANAGER_H
+
+#include <juce_dsp/juce_dsp.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+
+// include processor headers here
+#include "../Granular-Delay/GranularProcessor.h"
+#include "../Reverb/ReverbProcessor.h"
+#include "../Standard-Delay/DelayProcessor.h"
+#include "../Looper/LooperProcessor.h"
+
+// add #include directives above for additional processors as we add them
+
+
+class SignalPathManager : public juce::dsp::ProcessorBase
+{
+public:
+    SignalPathManager();
+    ~SignalPathManager() override = default;
+
+    void prepare(const juce::dsp::ProcessSpec& spec) override;
+    void reset() override;
+    void process(const juce::dsp::ProcessContextReplacing<float>& context) override;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SignalPathManager)
+};
+
+
+
+#endif //SIGNALPATHMANAGER_H
