5b85acbb7595c2fb0b36d12e5ae38f35b8535f75
smoke <gboling829@gmail.com>
2025-06-06 10:05:53 -0400

    added a checklist for adding new processors/classes to the plugin, added state storage (so that parameters are saved after closing the plugin)

---
+++ adding-processor-checklist.md
@@ -0,0 +1,9 @@
+# checklist of things to do when adding a new processor to the plugin
+
+- [ ] Add new processor class in AudioDSP folder (put the class in its own folder)
+- [ ] Register processor in ProcessorChain or in PluginProcessor
+- [ ] Define APVTS parameters
+- [ ] Attach parameters in PluginEditor
+- [ ] Add GUI controls (buttons, sliders)
+- [ ] Hook parameter values to processor behavior
+- [ ] Test and confirm parameter state saving
--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -419,14 +419,20 @@ void PluginProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
-    juce::ignoreUnused (destData);
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml(state.createXml());
+    copyXmlToBinary(*xml, destData);
 }
 
 void PluginProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
-    juce::ignoreUnused (data, sizeInBytes);
+    std::unique_ptr<juce::XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+    if (xmlState != nullptr && xmlState->hasTagName(apvts.state.getType()))
+    {
+        apvts.replaceState(juce::ValueTree::fromXml(*xmlState));
+    }
 }
 
 
